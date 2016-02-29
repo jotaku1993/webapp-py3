ad6a2384d4ff6e80af4944fb0885a92cd4a0e7f2
jotaku1993 <zl2442@columbia.edu>
2016-02-29 12:47:44 -0500

    able to post blog

---
+++ mysite/README.md
@@ -0,0 +1,2 @@
+Based on the python 2.7.10 and Django 1.8.2
+To build an easy blog
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
+
+class BlogsPost(models.Model):
+	#define the title and its length, body no limitation, timestamp
+	title = models.CharField(max_length = 150)
+	body = models.TextField()
+	timestamp = models.DateTimeField()
+
+admin.site.register(BlogsPost)
